5d857d0646425a72ae85db509aa9e28ac41fea90
Ragnar Kaldhand <mandrabura2001@gmail.com>
2025-02-17 21:57:36 +0400

    first commit

---
+++ DbF/Models/LibraryContext.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DbF.Models;
+
+public partial class LibraryContext : DbContext
+{
+    public LibraryContext()
+    {
+    }
+
+    public LibraryContext(DbContextOptions<LibraryContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Author> Authors { get; set; }
+
+    public virtual DbSet<Book> Books { get; set; }
+
+    public virtual DbSet<Publisher> Publishers { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=localhost,1433; Initial Catalog=Library; User ID=sa; Password=Password3454; TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Author>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Authors__3214EC07F74BA74B");
+
+            entity.Property(e => e.FirstName)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.LastName)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Book>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Books__3214EC07F9156CA1");
+
+            entity.Property(e => e.PublisherId).HasDefaultValue(1);
+            entity.Property(e => e.Title)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+
+            entity.HasOne(d => d.Author).WithMany(p => p.Books)
+                .HasForeignKey(d => d.AuthorId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Books__AuthorId__398D8EEE");
+
+            entity.HasOne(d => d.Publisher).WithMany(p => p.Books)
+                .HasForeignKey(d => d.PublisherId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Books_Publisher");
+        });
+
+        modelBuilder.Entity<Publisher>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Publishe__3214EC07F5FCFD93");
+
+            entity.ToTable("Publisher");
+
+            entity.Property(e => e.Address)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.PublisherName)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ DbF/Program.cs
@@ -0,0 +1,103 @@
+﻿namespace Program;
+
+using DbF.Models;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Program.AddAuthor(new Author{FirstName="Chuck", LastName="Palahniuk"});
+        Program.GetAllAuthors();
+    }
+
+    static void AddAuthor(Author author)
+    {
+        using (LibraryContext context = new LibraryContext())
+        {
+            context.Authors.Add(author);
+            context.SaveChanges();
+
+            Console.WriteLine($"New author added: {author.LastName}");
+        }
+    }
+
+    // static void GetAllAAuthors()
+    // {
+    //     using (LibraryContext context = new LibraryContext())
+    //     {
+    //         var au = context.Authors.Where((x) => x.LastName.StartsWith("A")).ToList();
+
+    //         foreach (var a in au)
+    //         {
+    //             Console.WriteLine(a.FirstName + " " + a.LastName);
+    //         } 
+    //     }
+    // }
+
+    static void GetAllAAuthors()
+    {
+        using (LibraryContext context = new LibraryContext())
+        {
+            var au = (from x in context.Authors
+                    where x.FirstName.StartsWith("A")
+                    select x).ToList();
+
+            foreach (var a in au)
+            {
+                Console.WriteLine(a.FirstName + " " + a.LastName);
+            } 
+        }
+    }
+
+    static void GetAllAuthors()
+    {
+        using (LibraryContext context = new LibraryContext())
+        {
+            var au = context.Authors.ToList();
+            foreach (var a in au)
+            {
+                Console.WriteLine(a.FirstName + " " + a.LastName);
+            }
+        }
+    }
+
+    // static Author GetAuthorByName(string fname)
+    // {
+    //     using (LibraryContext context = new LibraryContext())
+    //     {
+    //         var author = context.Authors.Where((x) => x.FirstName == fname).FirstOrDefault();
+    //         return author;
+    //     }
+    // }
+
+    static Author GetAuthorByName(string fname)
+    {
+        using (LibraryContext context = new LibraryContext ())
+        {
+            var author = (from x in context.Authors
+                        where x.FirstName == fname
+                        select x).FirstOrDefault();
+            return author;
+        }
+    }
+
+    // static Author GetAuthorById(int id)
+    // {
+    //     using (LibraryContext context = new LibraryContext ())
+    //     {
+    //         var author = context.Authors.Where((x) => x.Id == id).SingleOrDefault();
+    //         return author;
+    //     }
+    // }
+
+    static Author GetAuthorById(int id)
+    {
+        using (LibraryContext context = new LibraryContext ())
+        {
+            var author = (from x in context.Authors
+                        where x.Id == id
+                        select x).SingleOrDefault();
+            return author;
+        }
+    }
+}
